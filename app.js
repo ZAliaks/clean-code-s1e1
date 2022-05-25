13c376aaad007c8c4e78a66cee1ecfba7d4b164b
ZAliaks <zui@tut.by>
2022-05-25 13:52:31 +0300

    refactor: cnange tag-selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,21 +32,22 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className='list-item';
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='task task-label';    
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="del";
+    deleteButton.className="button del";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
-
 
     //and appending.
     listItem.appendChild(checkBox);
